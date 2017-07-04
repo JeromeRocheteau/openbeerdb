0e079d131874e391ddb4835521b37b6d11d76715
Jérôme Rocheteau <jerome.rocheteau@free.fr>
2017-07-04 10:04:03 +0200

    fix uppercase

--- src/main/resources/fr/icam/openbeerdb/queries/brewery-schema.sql
+++ src/main/resources/fr/icam/openbeerdb/queries/brewery-schema.sql
@@ -1,7 +1,7 @@
 create table `breweries` (
   `id` int(11) not null auto_increment,
   `name` varchar(45) not null,
-  `address` varchar(90) DEFAULT NULL,
+  `address` varchar(90) default null,
   `city` varchar(45) not null,
   `country` varchar(45) not null,
   primary key (`id`)
